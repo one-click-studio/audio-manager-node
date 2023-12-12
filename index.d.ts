0041c0addb3b6685ed8786080a2a253fcae966c6
Selgy <pct.neoz@gmail.com>
2023-12-12 11:09:13 +0100

    Update AudioDevice interface and package version

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,13 @@ declare interface AudioDevice {
     name: string;
     inputChannels: number;
     outputChannels: number;
+    host: string;
+}
+
+declare interface Device {
+    name: string;
+    inputChannels: number;
+    outputChannels: number;
 }
 
 declare interface AudioFeedback {
@@ -14,7 +21,7 @@ declare interface AudioFeedback {
 
 type SharedState = unknown;
 
-export declare function getAllDevices(): { inputs: AudioDevice['name'][], outputs: AudioDevice['name'][] };
-export declare function getDevice(name: AudioDevice['name']): AudioDevice;
+export declare function getAllDevices(): AudioDevice[];
+export declare function getDevice(name: AudioDevice['name']): Device;
 export declare function start(name: AudioDevice['name'], path: string|null, callback: (err: Error, data: AudioFeedback[]) => void): SharedState;
 export declare function stop(state: SharedState): void;
