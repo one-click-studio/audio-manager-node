d50e9ad9359a4f865e61dcc52141b650354e8c27
Selgy <pct.neoz@gmail.com>
2023-12-12 16:40:52 +0100

    Update audio-manager-node package version and add HostApi enum

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,21 @@
 /// <reference types="node" />
 
+// enum Host API for rust CPAL
+export enum HostApi {
+    ALSA = "ALSA",
+    COREAUDIO = "CoreAudio",
+    JACK = "JACK",
+    PULSEAUDIO = "PulseAudio",
+    ASIO = "ASIO",
+    WASAPI = "WASAPI",
+}
+
 declare interface AudioDevice {
     name: string;
     inputChannels: number;
     outputChannels: number;
-    host: string;
-}
-
-declare interface Device {
-    name: string;
-    inputChannels: number;
-    outputChannels: number;
+    host: HostApi;
+    error?: string;
 }
 
 declare interface AudioFeedback {
@@ -21,7 +26,8 @@ declare interface AudioFeedback {
 
 type SharedState = unknown;
 
+
 export declare function getAllDevices(): AudioDevice[];
-export declare function getDevice(name: AudioDevice['name']): Device;
-export declare function start(name: AudioDevice['name'], path: string|null, callback: (err: Error, data: AudioFeedback[]) => void): SharedState;
+export declare function getDevice(name: AudioDevice['name'], host: HostApi|undefined): AudioDevice;
+export declare function start(name: AudioDevice['name'], host: HostApi|undefined, path: string|null, callback: (err: Error, data: AudioFeedback[]) => void): SharedState;
 export declare function stop(state: SharedState): void;
