a2cdae9f1a0e1ace5b2d3084ff4c6ef41b022fcb
tillderoquefeuil <till-utils@proton.me>
2023-12-15 12:16:42 +0100

    Update start function signature to include optional gain parameter

--- index.d.ts
+++ index.d.ts
@@ -16,5 +16,5 @@ type SharedState = unknown;
 
 export declare function getAllDevices(): { inputs: AudioDevice['name'][], outputs: AudioDevice['name'][] };
 export declare function getDevice(name: AudioDevice['name']): AudioDevice;
-export declare function start(name: AudioDevice['name'], path: string|null, callback: (err: Error, data: AudioFeedback[]) => void): SharedState;
+export declare function start(name: AudioDevice['name'], path: string|null, callback: (err: Error, data: AudioFeedback[]) => void, gain?: number): SharedState;
 export declare function stop(state: SharedState): void;
